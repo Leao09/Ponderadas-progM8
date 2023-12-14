bd03a2a516db9588639e42f67736f524eb834de7
leao09 <felipe.leao@sou.inteli.edu.br>
2023-12-14 13:29:35 -0300

    Ponderada8

--- ponderada8/main.py
+++ ponderada8/main.py
@@ -3,44 +3,44 @@ import sys
 import speech_recognition as sr
 from googletrans import Translator
 from gtts import gTTS
+import playsound
 
-
-def transcribe_translate_speak(audio_path):
-    # Função para transcrever o áudio em texto
-    def transcribe_audio(audio_path):
-        recognizer = sr.Recognizer()
-        audio_file = sr.AudioFile(audio_path)
+def transcrever_traduzir_falar(caminho_audio):
+    # Função para realizar a transcrição do áudio para texto
+    def transcrever_audio(caminho_audio):
+        reconhecedor = sr.Recognizer()
+        arquivo_audio = sr.AudioFile(caminho_audio)
         
-        with audio_file as source:
-            audio_data = recognizer.record(source)
+        with arquivo_audio as fonte:
+            dados_audio = reconhecedor.record(fonte)
         
         try:
-            text = recognizer.recognize_google(audio_data, language='pt-BR')
-            return text
+            texto = reconhecedor.recognize_google(dados_audio, language='pt-BR')
+            return texto
         except sr.UnknownValueError:
             return "Não foi possível reconhecer a fala."
         except sr.RequestError:
             return "Erro na requisição ao serviço de reconhecimento de fala."
 
     # Função para traduzir o texto
-    def translate_text(text):
-        translator = Translator()
-        translation = translator.translate(text, src='pt', dest='en')
-        return translation.text
+    def traduzir_texto(texto):
+        tradutor = Translator()
+        traducao = tradutor.translate(texto, src='pt', dest='en')
+        return traducao.text
 
     # Função para converter texto em fala e reproduzir
-    def speak_text(text):
-        tts = gTTS(text=text, lang='en')
+    def reproduzir_texto(texto):
+        tts = gTTS(text=texto, lang='en')
         tts.save("videoplayback.mp3")
         playsound.playsound("videoplayback.mp3")
         os.remove("videoplayback.mp3")  # Remove o arquivo de áudio após reprodução
 
-    recognized_text = transcribe_audio(audio_path)
-    if recognized_text:
-        print("Texto reconhecido:", recognized_text)
-        translated_text = translate_text(recognized_text)
-        print("Texto traduzido:", translated_text)
-        speak_text(translated_text)
+    texto_reconhecido = transcrever_audio(caminho_audio)
+    if texto_reconhecido:
+        print("Texto reconhecido:", texto_reconhecido)
+        texto_traduzido = traduzir_texto(texto_reconhecido)
+        print("Texto traduzido:", texto_traduzido)
+        reproduzir_texto(texto_traduzido)
     else:
         print("Não foi possível reconhecer a fala no arquivo fornecido.")
 
@@ -48,5 +48,5 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Uso: python nome_do_arquivo.py caminho_para_o_arquivo_de_audio")
     else:
-        audio_file_path = sys.argv[1]
-        transcribe_translate_speak(audio_file_path)
+        caminho_arquivo_audio = sys.argv[1]
+        transcrever_traduzir_falar(caminho_arquivo_audio)
